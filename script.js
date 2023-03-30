88448302a5a44224f68ed1e51607fb1e6da83e66
Komal Panchal <95346969+PanchalKomalBalaji@users.noreply.github.com>
2023-03-30 15:12:53 +0530

    Calculator Task 4

--- script.js
+++ script.js
@@ -1,74 +1,28 @@
-const inputBox = document.querySelector(".inputField input");
-const addBtn = document.querySelector(".inputField button");
-const todoList = document.querySelector(".todoList");
-const deleteAllBtn = document.querySelector(".footer button");
-
-
-inputBox.onkeyup = () => {
-    let userEnteredValue = inputBox.value;
-    if (userEnteredValue.trim() != 0) {
-        addBtn.classList.add("active");
-    } else {
-        addBtn.classList.remove("active");
-    }
+// This function clear all the values
+function clearScreen() {    
+    document.getElementById("result").value = "";
+} 
+// This function display values
+function display(value) {    
+    document.getElementById("result").value += value;
+} 
+// This function evaluates the expression and returns result
+function calculate() {    
+    var p = document.getElementById("result").value;    
+    var q = eval(p);    
+    document.getElementById("result").value = q;
 }
 
-showTasks();
-
-addBtn.onclick = () => {
-    let userEnteredValue = inputBox.value;
-    let getLocalStorageData = localStorage.getItem("New Todo");
-    if (getLocalStorageData == null) {
-        listArray = [];
-    } else {
-        listArray = JSON.parse(getLocalStorageData);
-    }
-    listArray.push(userEnteredValue);
-    localStorage.setItem("New Todo", JSON.stringify(listArray));
-    showTasks();
-    addBtn.classList.remove("active");
+function clearScreen() {    
+    document.getElementById("result").value = "";
 }
 
-function showTasks() {
-    let getLocalStorageData = localStorage.getItem("New Todo");
-    if (getLocalStorageData == null) {
-        listArray = [];
-    } else {
-        listArray = JSON.parse(getLocalStorageData);
-    }
-    const pendingTasksNumb = document.querySelector(".pendingTasks");
-    pendingTasksNumb.textContent = listArray.length;
-    if (listArray.length > 0) {
-        deleteAllBtn.classList.add("active");
-    } else {
-        deleteAllBtn.classList.remove("active");
-    }
-    let newLiTag = "";
-    listArray.forEach((element, index) => {
-        newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-    });
-    todoList.innerHTML = newLiTag;
-    inputBox.value = "";
+function display(value) {    
+    document.getElementById("result").value += value;
 }
 
-
-function deleteTask(index) {
-    let getLocalStorageData = localStorage.getItem("New Todo");
-    listArray = JSON.parse(getLocalStorageData);
-    listArray.splice(index, 1);
-    localStorage.setItem("New Todo", JSON.stringify(listArray));
-    showTasks();
-}
-
-
-deleteAllBtn.onclick = () => {
-    let getLocalStorageData = localStorage.getItem("New Todo");
-    if (getLocalStorageData == null) {
-        listArray = [];
-    } else {
-        listArray = JSON.parse(getLocalStorageData);
-        listArray = [];
-    }
-    localStorage.setItem("New Todo", JSON.stringify(listArray));
-    showTasks();
+function calculate() {    
+    var p = document.getElementById("result").value;    
+    var q = eval(p);    
+    document.getElementById("result").value = q;
 }
